77fd30b29a5724302177a8f960ed6268476b530a
Daniel Johnsen <daniel.johnsen@knowit.no>
2024-06-26 15:41:09 +0200

    feat: add tests for all clients

---
+++ tests/deforestation.test.ts
@@ -0,0 +1,8 @@
+import { DeforestationClient } from "../src";
+
+const client = new DeforestationClient();
+
+test("fetches basin", async () => {
+  const result = await client.getBasin({ lat: 10, lon: 10 });
+  expect(result.data).toHaveProperty("features");
+});
---
+++ tests/geocoding.test.ts
@@ -0,0 +1,18 @@
+import { GeocoderClient } from "../src";
+
+const client = new GeocoderClient();
+
+test("fetching reverse geocode", async () => {
+  const result = await client.getReverseGeocoding({
+    lat: -1.79597,
+    lon: 29.922001,
+  });
+  expect(result.data).toHaveProperty("features");
+});
+
+test("fetching geocode", async () => {
+  const result = await client.getGeocoding({
+    q: "Berlin",
+  });
+  expect(result.data).toHaveProperty("features");
+});
