2310a6cd3b7641536f1094cf606169fe3351dbb2
Daniel Johnsen <daniel.johnsen@knowit.no>
2024-06-27 09:24:52 +0200

    add explanation for sed command

--- generate-types.sh
+++ generate-types.sh
@@ -6,6 +6,11 @@ openapi-typescript https://api-test.openepi.io/deforestation/openapi.json --outp
 openapi-typescript https://api-test.openepi.io/soil/openapi.json --output generated/soil.ts
 openapi-typescript https://api-test.openepi.io/crop-health/openapi.json --output generated/crop_health.ts
 
+
+# When generating types from the openapi spec, it makes the requestBody image-type "string".
+# However, when sending an image in javascript we want to send the raw data, which then means
+# that we should use the type "Buffer". The following sed command changes that in the generated file. 
+# Sed works differently on mac and non-mac machines, which is why we have an if statement.
 # Replace '"*/*": string;' with '"*/*": Buffer;'"
 if [ "$(uname)" = "Darwin" ]; then
     sed -i '' "s/\"\*\/\*\": string;/\"\*\/\*\": Buffer;/g" ./generated/crop_health.ts
